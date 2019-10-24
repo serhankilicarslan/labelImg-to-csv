e197897251bef01022c878d4837a6f83b48d0e52
serhankilicarslan <serhankilicarslan@gmail.com>
2019-10-24 18:03:32 +0300

    Bug fix.

--- index.js
+++ index.js
@@ -54,20 +54,21 @@ function addCsvLine(xml) {
 	var filename = xml.annotation.filename[0]
 	var width = xml.annotation.size[0].width[0]
 	var height = xml.annotation.size[0].height[0]
-	for (var i = 0;  i < xml.annotation.object.length; i++) {
-		var obj = xml.annotation.object[i];
-		var name = obj.name[0]
-		var xmin = obj.bndbox[0].xmin[0]
-		var ymin = obj.bndbox[0].ymin[0]
-		var xmax = obj.bndbox[0].xmax[0]
-		var ymax = obj.bndbox[0].ymax[0]
-		
-		var x1 = (xmin / width).toFixed(2)
-		var y1 = (ymin / height).toFixed(2)
-		var x2 = (xmax / width).toFixed(2)
-		var y2 = (ymax / height).toFixed(2)
+	if (xml.annotation.object != null)
+		for (var i = 0;  i < xml.annotation.object.length; i++) {
+			var obj = xml.annotation.object[i];
+			var name = obj.name[0]
+			var xmin = obj.bndbox[0].xmin[0]
+			var ymin = obj.bndbox[0].ymin[0]
+			var xmax = obj.bndbox[0].xmax[0]
+			var ymax = obj.bndbox[0].ymax[0]
+			
+			var x1 = (xmin / width).toFixed(2)
+			var y1 = (ymin / height).toFixed(2)
+			var x2 = (xmax / width).toFixed(2)
+			var y2 = (ymax / height).toFixed(2)
 
-		result += `\r\nTRAIN,${gsPath}/${folder}/${filename},${name},${x1},${y1},,,${x2},${y2},,`;
-	}
+			result += `\r\nTRAIN,${gsPath}/${folder}/${filename},${name},${x1},${y1},,,${x2},${y2},,`;
+		}
 	return result;
 }
